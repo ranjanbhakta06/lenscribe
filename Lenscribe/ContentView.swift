654c7b8596c6ae4162deb8be1717f22b35c8b598
Ranjan <ranjan.bhakta06@icloud.com>
2026-03-27 23:41:51 +0530

    Initial Commit

---
+++ Lenscribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lenscribe
+//
+//  Created by Ranjan Bhakta on 27/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lenscribe/LenscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenscribeApp.swift
+//  Lenscribe
+//
+//  Created by Ranjan Bhakta on 27/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LenscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
